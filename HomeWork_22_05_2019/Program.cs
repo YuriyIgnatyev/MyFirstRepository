cf18f53cf0b0abab9668b6485e0715355cfd5db2
YuriyIgnatyev <yualawyer@gmail.com>
2019-05-22 20:21:18 +0300

    Initial check-in of module HomeWork_22_05_2019

---
+++ HomeWork_22_05_2019/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HomeWork_22_05_2019
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            int a, b;
+            Console.WriteLine("Введите в одной строке делимое и делитель, затем нажмите <Enter>");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (b == 0) 
+            {
+                Console.WriteLine("Вы ошиблись. Делитель не должен быть равен нулю.");
+            }
+            else 
+            {
+                Console.WriteLine("Частное из введенных чисел составляет = " + (a / b)); 
+            }
+
+        }
+    }
+}
